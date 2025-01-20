2bd2d651dc4a9ce78d402f65ba9e24b412a6fce7
Your Name <you@example.com>
2025-01-20 15:58:25 +0700

    Updated config.py to differentite self.datasets

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ class ProjectPaths:
         self.data = self.root / "assets"
         self.outputs = self.root / "outputs"
 
+        self.ownership_data= self.data /"ownership-data"
+        self.brokerage_data = self.data /"brokerage-data"
+        self.marked_trades_data = self.data /"marked-trades-data"
+
     def get_data_file(self, filename):
         """Get path to data file as a string"""
         return str(self.data / filename)
